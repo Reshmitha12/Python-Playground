96a8cc06b0cf838b4e5f982d35adeb16f830e23b
Reshmitha <vallurureshmitha@gmail.com>
2024-03-14 23:50:39 +0530

    commiting

---
+++ disntict digit.py
@@ -0,0 +1,20 @@
+def dis(t):
+      ls=list(str(t))
+      for i in range(4):
+            for j in range(i+1):
+                  if ls[i]==ls[j]:
+                        return False
+      return True
+
+
+
+def p(n):
+      ans=False 
+      while(ans==False):
+            n=n+1
+            ans=dis(n)
+      return n 
+
+
+s=int(input())
+print(p(s))
